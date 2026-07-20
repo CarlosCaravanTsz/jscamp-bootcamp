d018450d381345a5a77d8fea4cf4eb4a6bb9800e
Carlos CaravanTsz <carloscara28@hotmail.com>
2026-07-19 21:07:43 -0600

    cerrando correcciones

--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -1,21 +1,10 @@
-/* Aquí va la lógica para mostrar los resultados de búsqueda */
 
 const jobListingSection = document.querySelector(".jobs-listings");
 
 fetch("./data.json")
   .then((response) => response.json())
   .then((jobs) => {
-    /*
-    Hacer un forEach para agregar elementos al DOM se puede traducir en hacer un repintado por cada elemento que iteramos.
-    Esto si bien no es un problema con pocos elementos, si puede ser costoso en rendimiento cuando tenemos muchos.
-    Una técnica es usar `createDocumentFragment`.
-    Supongamos que tenemos una caja de chocolates que tenemos que colocar en una góndola del supermercado:
-    Usar `createDocumentFragment` vendría a ser la caja, los chocolates cada elemento `li` y el llevarlo a la góndola (poner la caja ahí) es ejecutar `jobListingSection.appendChild(documentFragment);`. Que se traduce como: llevar la caja con chocolates al estante.
 
-    Si no usamos `createDocumentFragment`, lo que estaríamos haciendo es agarrar cada chocolate individualmente y llevarlo a la góndola uno por uno.
-
-    En vez de repintar el DOM por cada `li`, lo que hacemos es crearnos una caja virtual en donde vamos a colocar todos los `li`, y cuando tengamos toda esa estructura pronta, la agregamos a `jobListingSection` pintando una única vez.
-    */
     const documentFragment = document.createDocumentFragment()
 
     jobs.forEach((job) => {
--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -1,29 +1,19 @@
-/* Aquí va la lógica para filtrar los resultados de búsqueda */
 
-/* En JavaScript se recomienda por estándar usar camelCase y no snake_case. Cambie solo estos por practicidad, pero verás que en todos lados se utiliza así  */
 const filterUbicacion = document.getElementById("filter-location");
 const filterExperiencia = document.getElementById("filter-experience-level");
 const searchInput = document.getElementById("empleos-search-input");
 const filterTech = document.getElementById("filter-technology");
 
 
-/* Si la lógica es la misma, podemos simplificarlo en una sola función y reutilizarla */
 const handleSearchByFilter = () => {
   const jobs = document.querySelectorAll(".job-listing-card");
   const ubicacion_value = filterUbicacion.value;
-
-  // Actualizar los valores de los otros filtros
-  /* Si ya guardamos las referencias antes, no hace falta volverlas a guardar en otro puntero */
-  /* const filter_experiencia_updated = document.getElementById("filter-experience-level");
-  const filter_tech_updated = document.getElementById("filter-technology");
-  const search_input_updated = document.getElementById("empleos-search-input"); */
 
   const experiencia_value = filterExperiencia.value;
   const tech_value = filterTech.value;
   const query = searchInput.value.toLowerCase();
 
   if (!experiencia_value && !tech_value && !query)
-    // Podemos agregar un `return` y nos olvidamos de un `if/else`, queda mas claro
     return jobs.forEach((job) => {
       const modalidad = job.getAttribute("data-modalidad");
       const isShown = ubicacion_value == "" || ubicacion_value === modalidad;
@@ -36,7 +26,6 @@ const handleSearchByFilter = () => {
     const tech = job.getAttribute("data-tech");
     const title = normalizeTitleByJob(job)
 
-    // no queda tan claro de leer, podemos separar las validaciones por variables
     const isValidLocation = ubicacion_value == "" || ubicacion_value === modalidad
     const isValidExperience = experiencia_value == "" || experiencia_value === nivel
     const isValidTech = tech_value == "" || tech.includes(tech_value)
@@ -47,7 +36,6 @@ const handleSearchByFilter = () => {
   });
 }
 
-/* Como lo utilizamos en dos sitios, lo podemos pasar a una función */
 const normalizeTitleByJob = (job) => {
   return job
   .querySelector("h3")
@@ -68,12 +56,6 @@ searchInput.addEventListener("input", () => {
   const query = searchInput.value.toLowerCase();
   const jobs = document.querySelectorAll(".job-listing-card");
 
-  /* const filter_ubicacion_updated = document.getElementById("filter-location");
-  const filter_tech_updated = document.getElementById("filter-technology");
-  const filter_experiencia_updated = document.getElementById("filter-experience-level"); */
-
-  // SERIA BUENA IDEA RESETEAR LOS FILTROS SEARCH CUANDO ESCRIBE EN EL INPUT?
-  // O APLICAR TAMBIEN EL FILTRADO DE LOS OTROS 3 FILTROS MIENTRAS ESCRIBE?
   filterUbicacion.value = ""
   filterTech.value = ""
   filterExperiencia.value = ""
