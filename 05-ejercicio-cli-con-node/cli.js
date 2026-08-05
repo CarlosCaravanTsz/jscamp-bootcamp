a7227e2194586689cce32b7b3f44e1724c44c1d3
Carlos Caravantes <carloscara28@hotmail.com>
2026-08-04 23:17:54 -0600

    corrigiendo ejercicio 5

--- 05-ejercicio-cli-con-node/cli.js
+++ 05-ejercicio-cli-con-node/cli.js
@@ -4,8 +4,6 @@ import { join } from "node:path";
 const args = process.argv.slice(2);
 console.log("Arguments: ", args, typeof args);
 
-// Si damos por centado que el seguno argumento es el directorio a buscar, entonces podemos llegar a tener problemas si el usuario cambia el orden en el cli. Lo mejor es identificar el directorio y guardarlo de otra manera.
-// const dir = process.argv[2] ?? ".";
 const dir = args.find((arg) => !arg.startsWith("--")) ?? ".";
 
 const formatBytes = (size) => {
@@ -13,23 +11,6 @@ const formatBytes = (size) => {
   return `${(size / 1024).toFixed(2)} KB`;
 };
 
-// Está mal definido el `if`, en caso de que no pongamos ningún flag de permisos va a acceder a la primera condición y devolver los resultados de los ficheros.
-
-// Dos opciones:
-//1 . Condición con `&&`
-// if(process.permission && process.permission.has("fs.read", dir)) {
-//   // Código
-// }
-
-//2 . Condición con `.?`
-// if(process.permission?.has("fs.read", dir)) {
-//   // Código
-// }
-
-// Por otro lado, en vez de usar un `if` si el usuario tiene permisos, vamos a hacer el caso contrario (para evitar tener que escribir un `if` con un contenido tan grande).
-
-// Con esto, evitamos tener la lógica de negocio dentro de un `if`.
-// Y Siempre debemos dar al usuario junto con el error, la solución para que pueda ejecutar el comando correctamente. Es cuestión de UX
 if (!process.permission?.has("fs.read", dir)) {
   console.error(`No has podido leer el directorio ${dir}, por favor, habilita los permisos de lectura:
 
@@ -41,7 +22,6 @@ node --permission --allow-fs-read=${dir} cli.js ${dir}
 
 let files
 
-// Si el usuario quiere acceder a un directorio que no existe, el programa se rompe. Para evitarlo, podemos usar un `try/catch` para capturar el error y mostrar un mensaje más amigable al usuario.
 try {
   files = await readdir(dir);
 } catch (err) {
@@ -60,12 +40,9 @@ let entries = await Promise.all(
   }),
 );
 
-// Lo que hiciste está genial! Una cosa que podemos hacer para mejorar esto es hacerlo desde un lado más funcional, sin tener que evaluar con includes.
 
-// 1. Obtenemos todos los argumentos evitando duplicados
 const flags = new Set(args.filter((arg) => arg.startsWith("--")))
 
-// 2. creamos un objeto con los filtros y con los ordenadores
 const sorters = {
   asc: (a, b) => a.name.localeCompare(b.name),
   desc: (a, b) => b.name.localeCompare(a.name),
@@ -88,18 +65,6 @@ if(flags.has('--files') || flags.has('--folders')) {
   results = [...results].filter(filter)
 }
 
-
-/* if (args.includes("--asc"))
-  entries.sort((a, b) => a.name.localeCompare(b.name));
-else if (args.includes("--desc"))
-  entries.sort((a, b) => b.name.localeCompare(a.name));
-
-if (args.includes("--files"))
-  entries = entries.filter((entry) => !entry.isDir);
-else if (args.includes("--folders"))
-  entries = entries.filter((entry) => entry.isDir); */
-
-// Usamos console.log, pero te muestro otra alternativa con table, ambas estan muy bien:
 const logs = []
 for (const entry of results) {
   const icon = entry.isDir ? "📂" : "📄";
